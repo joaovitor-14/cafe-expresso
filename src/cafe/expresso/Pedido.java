190d8770848de267cd9e6a4f663361806c21d7c4
bibystar <gabrielly10xavier24@gmail.com>
2026-05-14 18:29:29 -0300

    Adiciona controle de fluxo de status na classe Pedido

--- src/cafe/expresso/Pedido.java
+++ src/cafe/expresso/Pedido.java
@@ -11,4 +11,15 @@ public class Pedido {
         this.itens = new ArrayList<>();
         this.status = StatusPedido.PENDENTE;
     }
+    public void adicionarItem(Produto produto, int quantidade) {
+        itens.add(new ItemPedido(produto, quantidade));
+    }
+
+    public double calcularTotal() {
+        double total = 0.0;
+        for (ItemPedido item : itens) {
+            total += item.calcularSubtotal();
+        }
+        return total;
+    }
 }
